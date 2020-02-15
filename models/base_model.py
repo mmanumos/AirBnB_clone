98e6e6e89eeb6649d719dbf578c50eda20212c6a
mmanumos <mosqueramanuel5@gmail.com>
2020-02-15 18:51:54 +0000

    [Feature] Exercise 3. Constructor, save, to_dict method are created. modified:   models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,42 +7,33 @@ from uuid import uuid4
 
 class BaseModel:
     """ defines all common attributes/methods for other classes """
-    def __init__(self, *args, **kwargs):
+
+    my_number = 0
+    name = ""
+
+    def __init__(self):
         """" constructor """
-        if len(kwargs) is 0:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.update_at = datetime.now()
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = kwargs.get(key)
-                if key == "created_at":
-                    self.created_at = datetime.strptime(kwargs.get(key),
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "updated_at":
-                    self.updated_at = datetime.strptime(kwargs.get(key),
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "my_number":
-                    self.my_number = kwargs.get(key)
-                if key == "name":
-                    self.name = kwargs.get(key)
+        self.id = str(uuid4())
+        self.created_at = (datetime.now()).isoformat()
+        self.update_at = (datetime.now()).isoformat()
+
+    def save(self):
+         """ Update the update_at"""
+         self.updated_at = (datetime.now()).isoformat()
+
 
     def __str__(self):
         """ __str__ """
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
-    def save(self):
-        """ Update the update_at """
-        self.updated_at = datetime.now()
-        models.storage.save()
-
     def to_dict(self):
         """ Returns a dictionary """
-        my_dict = self.__dict__.copy()
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict = {}
+        my_dict['my_number'] = self.my_number
+        my_dict['name'] = self.name
+        my_dict['created_at'] = self.created_at
+        my_dict['updated_at'] = self.updated_at
         my_dict['__class__'] = self.__class__.__name__
+        my_dict['id'] = self.id
         return(my_dict)
