a1e0a217030c3440516ffecbe183687f082dcdb5
1270holberton <1270@holbertonschool.com>
2020-02-18 18:36:32 -0500

    [Feature] Test to print modified:   models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -31,8 +31,7 @@ class BaseModel:
 
     def __str__(self):
         """ method that define print """
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
          """ method that updates the public instance attribute updated_at """
@@ -43,6 +42,6 @@ class BaseModel:
         """ method that returns a dictionary containing all keys/values of __dict__ of the instance """
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = (self.created_at).isoformat()
-        my_dict['updated_at'] = (self.updated_at).isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
         return(my_dict)
