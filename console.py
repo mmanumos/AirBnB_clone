be4fbde15e5210346db84d49ac380bfb41f385a2
mmanumos <mosqueramanuel5@gmail.com>
2020-02-18 08:35:43 +0000

    [Feature] The methods do_show and do_create are created. modified:   console.py

--- console.py
+++ console.py
@@ -1,13 +1,47 @@
 #!/usr/bin/python3
 """ shebang line - defines where the interpreter is located """
 import cmd
+import models
+from models.base_model import BaseModel
 """ import moduls """
 
 
 class HBNBCommand(cmd.Cmd):
     """ class with methods to work into the commands line """
     prompt = "(hbnb) "
+    list_class = ["BaseModel"]
+    list_err = ["** class name missing **", "** class doesn't exist **", "** instance id missing **", "** no instance found **" ]
+    
+    def do_create(self, line):
+        """ Create a new instance of BaseModel """
+        my_list = list(line.split())
+        if line == "":
+            print(self.list_err[0])
+        elif my_list[0] == "BaseModel":
+            obj = BaseModel()
+            print(obj.id)
+            obj.save()
+        else:
+            print(self.list_err[1])
 
+    def do_show(self, line):
+        """ Show object by id """
+        my_list = list(line.split())
+        if line == "":
+            print(self.list_err[0])
+        elif my_list[0] not in self.list_class:
+            print(self.list_err[1])
+        elif len(my_list) == 1:
+            print(self.list_err[2])
+        else:
+            my_dic = models.storage.all()
+            if ("BaseModel." + my_list[1]) in my_dic.keys():
+                print(my_dic["BaseModel." + my_list[1]])
+            else:
+                print(self.list_err[3])
+
+            
+            
     def do_quit(self, line):
         """ Quit command to exit the program """
         return True
