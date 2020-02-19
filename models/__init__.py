0a6f08a792156725fdb884fb7d2a7c17133243af
mmanumos <mosqueramanuel5@gmail.com>
2020-02-19 20:02:40 +0000

    [Fix] Pep8 Styles modified:   __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ shebang line - defines where the interpreter is located """
-""" init file in models """
 from models.engine.file_storage import FileStorage
 """ import moduls """
 
