e7ce1deec5da02e7383f75dd5866d3b7a43fd15e
ja-programmer <javierandres@tecnoayuda.co>
2020-02-19 16:00:19 +0000

    [Feature] Initial test for all files.

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" shebang line - defines where the interpreter is located """
+import unittest
+import pep8
+import models
+from models.base_model import BaseModel
+""" import moduls """
+class verify_pep8(unittest.TestCase):
+    """ class - PEP 8 validated """
+    def test_pep8(self):
+        """ method - PEP 8 test """
+        check = pep8.Checker("models/engine/file_storage.py", show_source=True)
+        file_error= check.check_all()
+
+class verify_work(unittest.TestCase):
+    """ funcionality test """
+
+    def setUp(self):
+        """ Method called immediately before calling the test method """
+        pass
+
+    def tearDown(self):
+        """ Method called immediately after calling the test method """
+        try:
+            remove("file.json")
+        except:
+            pass
